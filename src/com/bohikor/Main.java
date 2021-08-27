38aa0357ee0c82a880cd82b2d7f7495da9cad9f6
Tambua Evaristus <evaristustambua@gmail.com>
2021-08-27 13:18:39 +0100

    chapter 7 Array example

--- src/com/bohikor/Main.java
+++ src/com/bohikor/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        GradeBook book = new GradeBook("Biology");
+//        GradeBook book = new GradeBook("Biology");
 //        book.displaymessage();
 //        book.average();
 //
@@ -16,10 +16,27 @@ public class Main {
 //
 //        square.print();
 
-        Sum numbers = new Sum();
-        numbers.sum();
+//        Sum numbers = new Sum();
+//        numbers.sum();
+//
+//        Chrismas day =new Chrismas(5);
+//        day.printSong();
 
-        Chrismas day =new Chrismas(5);
-        day.printSong();
+        int[] age;
+        age = new int[5];
+        age[0] = 3;
+        age[1] = 4;
+        age[2] = 5;
+        age[3] = 6;
+        age[4] = 7;
+
+        System.out.println("The lenght of the array is"+ age.length);
+        System.out.println("Index  Value");
+        for (int i =0; i<age.length; i++){
+
+            System.out.println(i +"      "+age[i]);
+        }
+
+
     }
 }
