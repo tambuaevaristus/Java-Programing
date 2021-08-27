b7a742d4902d0362e9b964cef8e728f21d202cdb
Tambua Evaristus <evaristustambua@gmail.com>
2021-08-27 09:37:35 +0100

    Exercise 4.5

---
+++ src/com/bohikor/Sum.java
@@ -0,0 +1,15 @@
+package com.bohikor;
+
+public class Sum {
+    private int x = 1;
+    private int sum = 0;
+
+    public void sum(){
+//        while (x<=11) {
+            sum = sum + x;
+            System.out.println("The sum is" + sum);
+            x++;
+//        }
+    }
+
+}
