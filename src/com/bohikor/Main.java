c6ba22435145e4433a3002dd1a69492af59c6271
Tambua Evaristus <evaristustambua@gmail.com>
2021-08-28 10:09:45 +0100

    multidimentional array execise

--- src/com/bohikor/Main.java
+++ src/com/bohikor/Main.java
@@ -22,32 +22,13 @@ public class Main {
 //        Chrismas day =new Chrismas(5);
 //        day.printSong();
 
-        int[] age;
-        age = new int[5];
-        age[0] = 3;
-        age[1] = 4;
-        age[2] = 5;
-        age[3] = 6;
-        age[4] = 7;
+         int [][] array = {{3,4,2,5},{2,5,7,6}};
 
-        System.out.println("The lenght of the array is"+ age.length);
-
-        try {
-
-            for (int i =0; i<age.length; i++){
-                for (int j=0; j<=age[i]; j++){
-                    System.out.print('*');
-
-                }
-                System.out.println("");
-                age[15] = 5;
-                System.out.println("still in the try method");
-            }
-
-        }
-        catch ( ArrayIndexOutOfBoundsException e){
-            System.out.println("An Error occured");
-        }
-
+         for(int i=0; i<array.length; i++){
+             for (int j=0; j<array[i].length; j++){
+                 System.out.print(array[i][j]);
+             }
+             System.out.println();
+         }
     }
 }
